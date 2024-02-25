fd2135777ba0110360a9cdb4075065af300a9b8c
Cratis1831 <asotoudeh18@hotmail.com>
2024-02-25 08:22:35 -0500

    initial commit

---
+++ lib/models/todo.dart
@@ -0,0 +1,18 @@
+import 'package:isar/isar.dart';
+
+// dart run build_runner build
+part 'todo.g.dart';
+
+@Collection()
+class Todo {
+  Id id = Isar.autoIncrement;
+  final String title;
+  final String content;
+  final bool isCompleted;
+
+  Todo({
+    required this.title,
+    required this.content,
+    this.isCompleted = false,
+  });
+}
